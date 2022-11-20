f4ce4a3bd1b6ebab027874b8667bd9de0d61668d
bingual <bingual76@gmail.com>
2022-11-20 11:01:15 +0900

    Django-Rest-Framework

--- django_with_react_rev3/settings.py
+++ django_with_react_rev3/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1+d#58o7s$7hwn-mcnk98my1%m%61_-8wnycvua*#+f4%2t=b3"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
